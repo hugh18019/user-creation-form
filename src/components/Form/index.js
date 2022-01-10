f1ab1771a37351a96cdfc30cca74183cfa34d519
hugh18019 <hugh80082@gmail.com>
2022-01-10 15:03:29 -0800

    Added GET request and stored returned data in state

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,6 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 const Form = () => {
 
+    
+    const requestUrl = "https://frontend-take-home.fetchrewards.com/form";
+    const requestUrl2 = "https://jsonplaceholder.typicode.com/todos/1";
+
+    const [ occupations, setOccupations ] = useState([]);
+    const [ states, setStates ] = useState([]);
+
+
+
+    useEffect(() => {
+        fetch( requestUrl )
+                .then( function ( response ) {
+                    return response.json();
+                })
+                .then( function ( data ) {
+                    console.log( 'data', data );
+                    setOccupations( data.occupations );
+                    setStates( data.states );
+                })
+    }, [])
+    
+
     return (
         <div>
             This is the Form component
