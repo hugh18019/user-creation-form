76593d84fce74b70a0175ed8061b5dce46fef62b
hugh18019 <hugh80082@gmail.com>
2022-01-10 15:02:07 -0800

    Added input form for creating a new user

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Form = () => {
 
     return (
         <div>
             This is the Form component
+            <form>
+                <div>
+                    <label htmlFor="fullName">Full Name:</label>
+                    <input 
+                        name="fullName"
+                        type="fullName"
+                        id="fullName"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        name="email"
+                        type="email"
+                        id="email"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="pwd">Password:</label>
+                    <input 
+                        name="password"
+                        type="password"
+                        id="pwd"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="occupation">Occupation:</label>
+                    <select>
+                        <option>Choose</option>
+                        <option value='1'>One</option>
+                    </select>
+                </div>
+            </form>
+
         </div>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from '../../components/Form';
 
 const Home = () => {
 
     return (
         <div>
             This is the Home page
+            <Form />
         </div>
     )
 }
