dd0b35777b1313a6cf910eb0a181511e4d11582e
hugh18019 <hugh80082@gmail.com>
2022-01-11 12:55:33 -0800

    Fixed path to background image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Form from '../../components/Form';
-import vector from '../../assets/images/vector.png';
+// import vector from './assets/images/vector.png';
+// import vector from '../../../public/images/vector.png';
 
 const Home = () => {
 
     return (
-        <div id="home-container" style={{backgroundImage: "url(/images/vector.png)", width:"100vw", height: "100vh"}}>
-            {/* <img id="img" src={vector} alt="image" /> */}
+        <div id="home-container" style={{backgroundImage: "url(https://cdn.pixabay.com/photo/2021/07/15/08/43/abstract-6467847_1280.png)", 
+        backgroundSize: "cover", backgroundPosition: "center", width:"100vw", height: "100vh"}}>
             <Form />
         </div>
     )
